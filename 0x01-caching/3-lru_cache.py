9e851090cf06c1333c65b22c312ffe0c1e6830c3
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-31 00:39:28 +0300

    task-3.2

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -19,15 +19,15 @@ class LRUCache(BaseCaching):
     def put(self, key, item):
         """Put method."""
         if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS and\
-                    key not in self.cache_data:
-                lru_key = self.cache_data.popitem(last=False)[0]
-                print("DISCARD: {}".format(lru_key))
             self.cache_data[key] = item
             self.cache_data.move_to_end(key)
+            if len(self.cache_data) > self.MAX_ITEMS:
+                lru_key = self.cache_data.popitem(last=False)[0]
+                print("DISCARD: {}".format(lru_key))
 
     def get(self, key):
         """Get method."""
-        if key is not None:
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
             return self.cache_data.get(key, None)
         return None
