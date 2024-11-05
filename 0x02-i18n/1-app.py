9740bf81f0d72738e05ce79af3f2bab0710d11ea
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-05 23:02:23 +0200

    task 0 & 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""A basic Flask app setup."""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Config class that has a LANGUAGES class attribute."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+Babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """A / route index."""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
